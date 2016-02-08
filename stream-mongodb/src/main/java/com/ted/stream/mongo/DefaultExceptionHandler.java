d2e6b170d29b755f145b65f487c8650c464b624a
Daniel Knipping <daniel.knipping@prodyna.com>
2016-02-08 14:12:25 +0100

    exceptions geloggt

--- stream-mongodb/src/main/java/com/ted/stream/mongo/DefaultExceptionHandler.java
+++ stream-mongodb/src/main/java/com/ted/stream/mongo/DefaultExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ted.stream.mongo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,13 +12,17 @@ import com.ted.stream.mongo.exception.AccessDeniedException;
 @ControllerAdvice
 public class DefaultExceptionHandler {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultExceptionHandler.class);
+    
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(AccessDeniedException.class)
-    public void handleAccessDienedException() {
+    public void handleAccessDienedException(AccessDeniedException e) {
+        LOG.error("Access denied", e);
     }
     
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public void handlException() {
+    public void handlException(Exception e) {
+        LOG.error("Exception", e);
     }
 }
